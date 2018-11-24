d9f833014692ccbe2404aebf99b7700d5b457e47
testwangzheng <1157617038@qq.com>
2018-11-24 15:31:19 +0800

    改为本地文件

---
+++ script/test_002.py
@@ -0,0 +1,10 @@
+class Test_001:
+
+    def test_001_1(self):
+        assert True
+    def test_001_2(self):
+        assert True
+    def test_001_3(self):
+        assert False
+    def test_001_4(self):
+        assert False
